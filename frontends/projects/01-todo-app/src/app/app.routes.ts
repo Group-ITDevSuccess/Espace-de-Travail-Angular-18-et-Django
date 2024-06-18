f907ebb6ec1c42b9bcb1ae8488a79c52f100ba72
TSIDIANY Raharison Muriel <106076190+ITDevSuccess@users.noreply.github.com>
2024-06-18 20:42:14 +0300

    Mise en place de templates auths et router auths

--- frontends/projects/01-todo-app/src/app/app.routes.ts
+++ frontends/projects/01-todo-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'accounts',
+    loadComponent: () => import('./components/auth/auth.component') ,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./components/auth/login.component')
+      },
+      {
+        path: 'signin',
+        loadComponent: () => import('./components/auth/signin.component')
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
